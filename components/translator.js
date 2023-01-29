3055a6c0b2744ff65a3e991240be93296fbd237c
vkats90 <mars77@gmail.com>
2023-01-29 11:34:04 -0700

    feat: functionality is working and most FCC tests are passing

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,65 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
-
+  translate(phrase, mode) {
+    const innitial = phrase;
+    if (mode == "american-to-british") {
+      for (let word in americanOnly) {
+        phrase = phrase.replace(
+          new RegExp(word, "ig"),
+          '<span class="highlight">' + americanOnly[word] + "</span>"
+        );
+      }
+      for (let word in americanToBritishSpelling) {
+        phrase = phrase.replace(
+          new RegExp(word, "ig"),
+          '<span class="highlight">' +
+            americanToBritishSpelling[word] +
+            "</span>"
+        );
+      }
+      for (let word in americanToBritishTitles) {
+        phrase = phrase.replace(
+          new RegExp(word, "ig"),
+          '<span class="highlight">' + americanToBritishTitles[word] + "</span>"
+        );
+      }
+      phrase = phrase.replace(
+        /(\d+)\:(\d+)/gm,
+        '<span class="highlight">$1.$2</span>'
+      );
+    }
+    if (mode == "british-to-american") {
+      for (let word in britishOnly) {
+        phrase = phrase.replace(
+          new RegExp(word, "ig"),
+          '<span class="highlight">' + britishOnly[word] + "</span>"
+        );
+      }
+      for (let word in americanToBritishSpelling) {
+        phrase = phrase.replace(
+          new RegExp(americanToBritishSpelling[word], "ig"),
+          '<span class="highlight">' + word + "</span>"
+        );
+      }
+      for (let word in americanToBritishTitles) {
+        phrase = phrase.replace(
+          new RegExp(americanToBritishTitles[word] + " ", "ig"),
+          '<span class="highlight">' + word + "</span>"
+        );
+      }
+      phrase = phrase.replace(
+        /(\d+)\.(\d+)/gm,
+        '<span class="highlight">$1:$2</span>'
+      );
+    }
+    console.log(phrase);
+    if (innitial == phrase) return "Everything looks good to me!";
+    return phrase;
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,29 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    console.log(req.body);
+    switch (true) {
+      case !req.body.locale:
+        res.json({ error: "Required field(s) missing" });
+        break;
+      case !req.body.text:
+        res.json({ error: "No text to translate" });
+        break;
+      case req.body.locale != "american-to-british" &&
+        req.body.locale != "british-to-american":
+        res.json({ error: "Invalid value for locale field" });
+        break;
+      default:
+        res.json({
+          text: req.body.text,
+          translation: translator.translate(req.body.text, req.body.locale),
+        });
+        break;
+    }
+  });
 };
